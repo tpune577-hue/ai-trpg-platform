bfd48c4ba6f0478ea2b6ee908abdb93d9faaf9f3
tpune577-hue <tpune577@gmail.com>
2026-01-31 21:43:04 +0700

    Update ItemDetailModal.tsx

--- components/marketplace/ItemDetailModal.tsx
+++ components/marketplace/ItemDetailModal.tsx
@@ -13,7 +13,7 @@ interface ItemDetailModalProps {
         price: number
         imageUrl: string
         creatorName: string
-        downloads: number
+        downloads?: number // Made optional to match MarketplaceItem
         rating?: number
         tags?: string[]
     } | null
@@ -139,12 +139,14 @@ export default function ItemDetailModal({
                                 <span className="text-white font-semibold">{item.rating.toFixed(1)}</span>
                             </div>
                         )}
-                        <div className="flex items-center gap-2 text-gray-400">
-                            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                                <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
-                            </svg>
-                            <span>{item.downloads} downloads</span>
-                        </div>
+                        {item.downloads !== undefined && (
+                            <div className="flex items-center gap-2 text-gray-400">
+                                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                                    <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
+                                </svg>
+                                <span>{item.downloads} downloads</span>
+                            </div>
+                        )}
                     </div>
 
                     {/* Description */}
